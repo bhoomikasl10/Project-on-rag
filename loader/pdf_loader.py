771da9f77fdd1b60b32d2899049da9058cf2f240
bhoomikasl10 <slbhoomika0603@gmail.com>
2026-08-11 16:09:09 +0530

    Add multiple PDF support for Level 2 Task 1

--- loader/pdf_loader.py
+++ loader/pdf_loader.py
@@ -1,13 +1,26 @@
 from langchain_community.document_loaders import PyPDFLoader
+import os
 
 
 class PDFLoader:
 
     @staticmethod
     def load_pdf(pdf_path: str):
-
         loader = PyPDFLoader(pdf_path)
-
         documents = loader.load()
-
         return documents
+
+    @staticmethod
+    def load_multiple_pdfs(pdf_directory: str):
+        all_documents = []
+
+        for filename in os.listdir(pdf_directory):
+            if filename.lower().endswith(".pdf"):
+                pdf_path = os.path.join(pdf_directory, filename)
+
+                loader = PyPDFLoader(pdf_path)
+                documents = loader.load()
+
+                all_documents.extend(documents)
+
+        return all_documents
--- test_vector_db.py
+++ test_vector_db.py
@@ -1,7 +1,9 @@
-from loaders.pdf_loader import PDFLoader
+from loader.pdf_loader import PDFLoader
 from services.vector_db import VectorDB
 
-docs = PDFLoader.load_pdf(r"C:\Users\Administrator\Bhoomi\data\Bhoomika S L 2027.pdf")
+docs = PDFLoader.load_multiple_pdfs(
+    r"C:\Users\Administrator\Bhoomi\data"
+)
 
 db = VectorDB.create(docs)
 
